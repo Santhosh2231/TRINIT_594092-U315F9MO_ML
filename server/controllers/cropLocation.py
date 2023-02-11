14563d7fe8219e3b8531bfef6bf621ec412b9786
Santhosh Raminedi <87355988+Santhosh2231@users.noreply.github.com>
2023-02-11 11:44:54 +0530

    v7 - Start of react work

--- server/controllers/cropLocation.py
+++ server/controllers/cropLocation.py
@@ -1,6 +1,91 @@
 
 from flask_restful import Resource
+from flask import request,Response
+import pandas as pd
+import numpy as np;
+from pathlib import Path
+from math import sqrt;
+import json
+
+def euclidean_distance(row1, row2):
+    distance = (row1[0]-row2[6])**2
+    return sqrt(distance)
+ 
+# Locate the most similar neighbors
+def get_neighbors(train, test_row, num_neighbors):
+    distances = list()
+    for train_row in train:
+        #print(train_row)
+        dist = euclidean_distance(test_row, train_row)
+        distances.append((train_row, dist))
+    distances.sort(key=lambda tup: tup[1])
+    #print(distances)
+    neighbors = list()
+    for i in range(num_neighbors):
+        neighbors.append(distances[i][0])
+    return neighbors
 
 class cropLocation(Resource):
-    def get(self):
-        return {"val":10}
+    def post(self):
+        body = request.get_json()
+        location = body["location"]
+        season = body["season"]
+        value = 1;
+        
+        if season=="Jan-Feb":
+            value=2
+        elif season=="Mar-May":
+            value=3
+        elif season=="Jun-Sep":
+            value=4
+        elif season=="Oct-Dec":
+            value=3
+        elif season=="ANNUAL":
+            value=12
+        else:
+            value=1
+
+        path = Path.cwd()
+        path_1 = str(path)+"\\..\\Dataset\\"+"Commodity_prices"
+        df_commodity = pd.read_csv(filepath_or_buffer=path_1)
+        path_2 = str(path)+"\\..\\Dataset\\"+"Crop_recommendation.csv";
+        df_crop = pd.read_csv(path_2)
+        path_3 = str(path)+"\\..\\Dataset\\"+"district wise rainfall normal.csv"
+        df_rainfall = pd.read_csv(path_3)
+        df_crop["label"] = df_crop["label"].replace( ["rice","papaya","watermelon","muskmelon","apple","orange","coconut","jute","grapes","mango","banana","pomegranate","lentil","blackgram","maize","mungbean","mothbeans","coffee","cotton","pigeonpeas","kidneybeans","chickpea"],["Rice","Papaya","Water Melon","Karbuja(Musk Melon)","Apple","Orange","Coconut","Jute","Grapes","Mango","Banana","Pomegranate","Lentil (Masur)(Whole)","Black Gram (Urd Beans)(Whole)","Maize","Green Gram (Moong)(Whole)","Moath Dal","Tomato","Cotton","Pegeon Pea (Arhar Fali)","Niger Seed (Ramtil)","Bengal Gram Dal (Chana Dal)"])
+        Crops = df_crop["label"].unique()
+        df_commodity = df_commodity[df_commodity["commodity"].isin(Crops)]
+        test = df_rainfall[df_rainfall["DISTRICT"]==location][season].to_numpy()
+        test = test/value;
+        dataset = df_crop.to_numpy()
+        neighbors = get_neighbors(dataset, test, 100)
+        pred=[]
+        print(test)
+        for neighbor in neighbors:
+            pred.append(neighbor[-1])
+
+        val = np.unique(pred)
+        data = df_commodity[df_commodity["commodity"].isin(val)]
+        dict = {}
+        for i in val:
+            max_val = max(data[data["commodity"]==i]["max_price"])
+            min_val = max(data[data["commodity"]==i]["min_price"])
+            min_modal_price = min(data[data["commodity"]==i]["modal_price"])
+            max_modal_price = max(data[data["commodity"]==i]["modal_price"])
+            avg_modal_price = data[data["commodity"]==i]["modal_price"].mean()
+            dict[i] = {"Max Val":max_val,"Min Val":min_val,"Min Modal Price":min_modal_price,"Max Modal Price":max_modal_price,"Avg modal price":avg_modal_price}
+
+        df = pd.DataFrame.from_dict(dict).transpose() 
+        context = df.sort_values("Avg modal price", ascending=False).to_json()
+        context = json.loads(context)
+        
+        return {'context':context},200
+
+        
+                                
+
+
+
+
+        
+        
--- server/controllers/cropSoil.py
+++ server/controllers/cropSoil.py
@@ -1,10 +1,87 @@
+import sys
+# setting path
+sys.path.append('../models')
+from flask import request,Response
 from flask_restful import Resource
+# from models import soil_crop
+import joblib
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import json
+from math import sqrt
+
+
+def euclidean_distance(row1, row2):
+        distance = 0.0
+        for i in range(len(row1)-1):
+            distance += (row1[i] - row2[i])**2
+        return sqrt(distance)
+ 
+    # Locate the most similar neighbors
+def get_neighbors(train, test_row, num_neighbors):
+    distances = list()
+    for train_row in train:
+        dist = euclidean_distance(test_row, train_row)
+        distances.append((train_row, dist))
+    distances.sort(key=lambda tup: tup[1])
+    neighbors = list()
+    for i in range(num_neighbors):
+        neighbors.append(distances[i][0])
+        # print(neighbors[-1])
+
+    return neighbors
 
 class soilcrop(Resource):
-    def get(self):
-        return {"val":10}
+    def post(self):
+        body = request.get_json()
+        print(body)
+        N = float(body["N"])
+        P = float(body["P"])
+        K = float(body["K"])
+        temp = float(body["temperature"])
+        humidity = float(body["humidity"])
+        ph = float(body["ph"])
+        rainfall = float(body["rainfall"])
+        test_data = np.array([[N,P,K,temp,humidity,ph,rainfall]])
+        data = pd.DataFrame(test_data)
+
+        path = Path.cwd()
+        path_2 = str(path)+"\
+            +\..\\Dataset\\"+"soil_crop.pkl"
+        print(path_2)
+        SVC_from_joblib = joblib.load(path_2)
+        y_pred = SVC_from_joblib.predict(data)
+        print(y_pred)
+        path_2 = str(path)+"\\..\\Dataset\\"+"Crop_recommendation.csv"
+        dataset=pd.read_csv(path_2).to_numpy()
+        print(test_data[0])
+        neighbors = get_neighbors(dataset, test_data[0], 120)
+        pred=[]
+        for neighbor in neighbors:
+            pred.append(neighbor[-1])
+        # print(pred)
+        list1 = np.unique(pred).tolist();
+        # print(y_pred)
+        # print(list1)
+        list1.remove(y_pred)
+        res = {
+            "main":[y_pred[0]],
+            "alternative":list1
+        }
+        res=json.dumps(res)
+        res=json.loads(res)
+        
+        return {'context':res}, 200
+    
 
 
 class SoilAnalysis(Resource):
     def get(self):
-        return {"val":10}
+        path_2 = Path.cwd()
+        path_2 = str(path_2)+"\\..\\Dataset\\"+"Crop_recommendation.csv"
+        df=pd.read_csv(path_2)
+        context = pd.merge(df.groupby(['label'])['temperature', 'humidity', 'ph', 'rainfall'].min().round(0),df.groupby(['label'])['temperature', 'humidity', 'ph', 'rainfall'].max().round(0),on="label").transpose().to_json()
+        context = json.loads(context)
+        
+        return {'context':context},200
